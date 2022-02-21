f88d4bd0a7c6e963f2f1a0bda7aa5f27ed2a68f8
Sharmin Moon <sharminmoon0308@gmail.com>
2022-02-21 21:31:31 +0600

    orderitems model done

--- modules/orders/order.item.model.js
+++ modules/orders/order.item.model.js
@@ -8,3 +8,14 @@ variations,
 quantity,
 price,
 */
+const mongoose = require("mongoose");
+const orderItemSchema = new mongoose.Schema({
+    orderId : {type: Schema.Types.String, required:true},
+    productId: {type: Schema.Types.String, required:true},
+    name: {type: Schema.Types.String, trim: true, required: true},
+    description: {type: Schema.Types.String, trim: true},
+    variations: {type: Schema.Types.Array,  required: true},
+    quantity: {type: Schema.Types.Number,  required: true},
+    price: {type: Schema.Types.Number,  required: true}    
+}); 
+module.exports = mongoose.model("orderItems", orderItemSchema); 
